e75d133d3420d4f1b1dcb608ba4ee6e61808d34a
Stéphane <sfleury72@gmail.com>
2019-01-24 16:54:51 +0100

    Commit initial

---
+++ src/main/java/com/sfl/gestibud/rest/HelloEndpoint.java
@@ -0,0 +1,17 @@
+package com.sfl.gestibud.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.awt.*;
+
+@Path("hello")
+@Produces(MediaType.TEXT_PLAIN)
+public class HelloEndpoint {
+    @GET
+    public Response testGbdgApi() {
+        return Response.ok("This is financial home managment...").build();
+    }
+}
---
+++ src/main/java/com/sfl/gestibud/rest/RestApplication.java
@@ -0,0 +1,10 @@
+package com.sfl.gestibud.rest;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("gbdgapi")
+public class RestApplication extends Application {
+    public RestApplication() { super();}
+}
